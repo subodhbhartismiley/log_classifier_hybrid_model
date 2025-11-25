519ea33ab4f2b3e8deb83a935d04978bbd8e4e4b
subodhbhartismiley <111581324+subodhbhartismiley@users.noreply.github.com>
2025-11-26 04:03:40 +0530

    Comment out main execution block
    Comment out the main execution block for testing purposes.

--- classifier_lr_bert.py
+++ classifier_lr_bert.py
@@ -15,15 +15,15 @@ def classify_with_lr_bert(log_message):
     return predicted_label
 
 
-if __name__ == "__main__":
-    logs = [
-        "alpha.osapi_compute.wsgi.server - 12.10.11.1 - API returned 404 not found error",
-        "GET /v2/3454/servers/detail HTTP/1.1 RCODE   404 len: 1583 time: 0.1878400",
-        "System crashed due to drivers errors when restarting the server",
-        "Hey bro, chill ya!",
-        "Multiple login failures occurred on user 6454 account",
-        "Server A790 was restarted unexpectedly during the process of data transfer"
-    ]
-    for log in logs:
-        label = classify_with_bert(log)
-        print(log, "->", label)
+# if __name__ == "__main__":
+#     logs = [
+#         "alpha.osapi_compute.wsgi.server - 12.10.11.1 - API returned 404 not found error",
+#         "GET /v2/3454/servers/detail HTTP/1.1 RCODE   404 len: 1583 time: 0.1878400",
+#         "System crashed due to drivers errors when restarting the server",
+#         "Hey bro, chill ya!",
+#         "Multiple login failures occurred on user 6454 account",
+#         "Server A790 was restarted unexpectedly during the process of data transfer"
+#     ]
+#     for log in logs:
+#         label = classify_with_bert(log)
+#         print(log, "->", label)
